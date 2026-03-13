e24bf770c3936f882b1da0409bc2f1002678675a
gabushital <gabushital537@gmail.com>
2026-03-12 20:11:53 -0700

    Add files via upload

---
+++ color.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>
+            color example
+        </title>
+    </head>
+    <body>
+        <?php
+        $color="red";
+        echo"car is ".$color;
+        ?>
+    </body>
+</html>
