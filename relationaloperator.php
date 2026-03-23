49a68e76edd273a353bd712d24b866ff63b6903e
gabushital <gabushital537@gmail.com>
2026-03-22 21:04:10 -0700

    Add files via upload

---
+++ relationaloperator.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>
+            Logical Operator
+        </title>
+    </head>
+<body>
+    <?php
+    $a=20;
+    $b=10;
+    echo">:".($a>$b);
+    echo"<br>>=:".($a>=$b);
+    echo"<br><:".($a<$b);
+    echo"<br><=:".($a<=$b);
+    echo"<br>!=:".($a!=$b);
+    ?>
+</body>
+</html>
